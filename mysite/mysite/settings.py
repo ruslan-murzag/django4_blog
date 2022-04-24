8a58c1b31ad3150d44dd03cefb0a81a09b9be52b
Ruslan <rus.mur2001@gmail.com>
2022-04-24 14:35:37 +0600

    Create the requerments

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'blog',
         'USER': 'blog',
-        'PASSWORD': '502232',
+        'PASSWORD': 'blog',
     }
 }
 
